baba1ac58ebc21f754fa50838915afc457e6d411
cpear01 <cpearson_su@yahoo.com>
2017-08-18 19:08:09 -0400

    added practice problems

--- Tutoring_8_18_17/Program.cs
+++ Tutoring_8_18_17/Program.cs
@@ -10,6 +10,35 @@ namespace Tutoring_8_18_17
     {
         static void Main(string[] args)
         {
+            float salad = 5.12f;
+            float totalNow = 27.10f;
+            Console.WriteLine(totalNow + salad);
+
+            double monty = 16.72d;
+            double larry = 4.12d;
+            Console.WriteLine(monty + larry);
+
+            int total = 72;
+            int totalNumbers = 3;
+            Console.WriteLine(total / totalNumbers - 1);
+
+            int middleNum = (72 / 3);
+            int smallNum = (middleNum - 1);
+            int largeNum = (middleNum + 1);
+            Console.WriteLine(smallNum);
+            Console.WriteLine(middleNum);
+            Console.WriteLine(largeNum);
+
+
+            int middle_Num = (288 / 3);
+            int small_Num = (middleNum - 1);
+            int large_Num = (middleNum + 1);
+            Console.WriteLine(smallNum);
+            Console.WriteLine(middleNum);
+            Console.WriteLine(largeNum);
+
+
+
         }
     }
 }
